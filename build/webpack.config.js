7540ce5d909bc2f3fd054ccf323a7c7284f29d4b
Smilefounder <i.love.to.smile.around@gmail.com>
2019-12-25 12:05:26 +0700

    upd pwa

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: {
     shards: './src/js/index.js',
-    shardsdashboards: './src/js/dashboards/shards-dashboards.js',
+    shardsDashboards: './src/js/dashboards/shards-dashboards.js',
     demo: './src/extras/js/shards-demo.js'
   },
   target: 'web',
